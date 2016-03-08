c763ac4c44d0c3606778589c699cdf3e28e4bbce
Arthurisme <is@live.ca>
2016-03-07 22:39:21 -0500

    201603072207

--- 3.1.3/qotd/grails-app/controllers/qotd/QuoteController.groovy
+++ 3.1.3/qotd/grails-app/controllers/qotd/QuoteController.groovy
@@ -2,6 +2,9 @@ package qotd
 
 class QuoteController {
 
+    def quoteService
+
+
     static scaffold = Quote
 
 //    def index() { }
@@ -14,37 +17,49 @@ class QuoteController {
         render "<h1>Love is the most beautiful language, It is the God</h1>"
     }
 
-
-    def random() {
-
-        def allQuotes = Quote.list()
-        def randomQuote
-
-        if (allQuotes.size() > 0)
-        {
-            def randomIdx = new Random().nextInt(allQuotes.size())
-            randomQuote = allQuotes[randomIdx]
-        }else
-        {
-            randomQuote = new Quote(author: "Anonymous",
-                    content: "Real Programmers don't eat much Quiche")
-
-        }
-
-
-        [ quote: randomQuote ]
-    }
-
 //    def random() {
 //        def staticAuthor = "Anonymous"
 //        def staticContent = "Real Programmers don't eat much quiche"
 //        [ author: staticAuthor, content: staticContent]
 //    }
 
+    def random() {
+
+//        def allQuotes = Quote.list()
+//        def randomQuote
+//
+//        if (allQuotes.size() > 0){
+//
+//            def randomIdx = new Random().nextInt(allQuotes.size())
+//            randomQuote = allQuotes[randomIdx]
+//
+//        }else{
+//              randomQuote = new Quote(author: "Anonymous",
+//                    content: "Real Programmers don't eat much Quiche")        }
+//        [ quote : randomQuote]
+
+//test service:
+        def randomQuoteReturnFromService = quoteService.getRandomQuote()
+
+
+
+        [quote: randomQuoteReturnFromService]
+
+    }
+
     def random2() {
         def staticAuthor = "Anonymous"
         def staticContent = "Real Programmers don't eat much quiche"
-        [ author: staticAuthor, content: staticContent]
+        [author: staticAuthor, content: staticContent]
+    }
+
+
+    def ajaxRandom() {
+        def randomQuote = quoteService.getRandomQuote()
+        render {
+            q(randomQuote.content)
+            p(randomQuote.author)
+        }
     }
 
 }
--- 3.1.3/qotd/grails-app/services/qotd/QuoteService.groovy
+++ 3.1.3/qotd/grails-app/services/qotd/QuoteService.groovy
@@ -8,4 +8,32 @@ class QuoteService {
     def serviceMethod() {
 
     }
+
+    def getStaticQuote() {
+        return new Quote(author: "Anonymous",
+                content: "Real Programmers Don't eat much quicheQuiche")
+    }
+
+    def getRandomQuote(){
+
+        def allQuotes = Quote.list()
+        def randomQuote
+
+        if (allQuotes.size() > 0){
+
+            def randomIdx = new Random().nextInt(allQuotes.size())
+            randomQuote = allQuotes[randomIdx]
+
+        }else{
+            randomQuote = new Quote(author: "Anonymous",
+                    content: "Real Programmers don't eat much Quiche")        }
+
+
+
+
+
+        return randomQuote;
+    }
+
+
 }
