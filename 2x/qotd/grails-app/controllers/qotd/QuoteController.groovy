233599d23566d1471c1e25ea022e99f4b4eb6432
Arthurisme <is@live.ca>
2016-03-07 21:43:08 -0500

    201603072127

--- 2x/qotd/grails-app/controllers/qotd/QuoteController.groovy
+++ 2x/qotd/grails-app/controllers/qotd/QuoteController.groovy
@@ -2,6 +2,9 @@ package qotd
 
 
 class QuoteController {
+
+    def quoteService
+
     static scaffold = true
 
 //    def index() { }
@@ -22,29 +25,43 @@ class QuoteController {
 
     def random() {
 
-        def allQuotes = Quote.list()
-        def randomQuote
 
-        if (allQuotes.size() > 0){
+//        def allQuotes = Quote.list()
+//        def randomQuote
+//
+//        if (allQuotes.size() > 0){
+//
+//            def randomIdx = new Random().nextInt(allQuotes.size())
+//            randomQuote = allQuotes[randomIdx]
+//
+//        }else{
+//              randomQuote = new Quote(author: "Anonymous",
+//                    content: "Real Programmers don't eat much Quiche")        }
+//        [ quote : randomQuote]
 
-            def randomIdx = new Random().nextInt(allQuotes.size())
-            randomQuote = allQuotes[randomIdx]
 
-        }else{
-              randomQuote = new Quote(author: "Anonymous",
-                    content: "Real Programmers don't eat much Quiche")        }
+//test service:
+        def randomQuoteReturnFromService = quoteService.getRandomQuote()
 
 
 
+        [ quote : randomQuoteReturnFromService]
 
-
-        [ quote : randomQuote]
     }
 
     def random2() {
         def staticAuthor = "Anonymous"
         def staticContent = "Real Programmers don't eat much quiche"
         [ author: staticAuthor, content: staticContent]
+    }
+
+
+    def ajaxRandom() {
+        def randomQuote = quoteService.getRandomQuote()
+        render {
+            q(randomQuote.content)
+            p(randomQuote.author)
+        }
     }
 
 }
--- 2x/qotd/grails-app/views/quote/random.gsp
+++ 2x/qotd/grails-app/views/quote/random.gsp
@@ -9,11 +9,28 @@
 <html>
 <head>
     <title>Random Quote</title>
+    <g:javascript library="jquery" />
 </head>
 <body>
 <div id="quote">
     <q>${quote.content}</q>
     <p>${quote.author}</p>
+
+
+
+    <ul id="menu">
+        <li>
+            <g:remoteLink action="ajaxRandom" update="quote">
+                Next Quote
+            </g:remoteLink>
+        </li>
+        <li>
+            <g:link action="index">
+                Admin
+            </g:link>
+        </li>
+    </ul>
+
 </div>
 </body>
 </html>
