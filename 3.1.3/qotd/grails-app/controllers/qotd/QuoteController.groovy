dc460a2c9ae6d6b6889e766d17e0d49754febe35
Arthurisme <is@live.ca>
2016-03-07 22:08:13 -0500

    201603072207

---
+++ 3.1.3/qotd/grails-app/controllers/qotd/QuoteController.groovy
@@ -0,0 +1,50 @@
+package qotd
+
+class QuoteController {
+
+    static scaffold = Quote
+
+//    def index() { }
+
+    def home() {
+        render "<h1>Real Programmers do not eat Quiche</h1>"
+    }
+
+    def love() {
+        render "<h1>Love is the most beautiful language, It is the God</h1>"
+    }
+
+
+    def random() {
+
+        def allQuotes = Quote.list()
+        def randomQuote
+
+        if (allQuotes.size() > 0)
+        {
+            def randomIdx = new Random().nextInt(allQuotes.size())
+            randomQuote = allQuotes[randomIdx]
+        }else
+        {
+            randomQuote = new Quote(author: "Anonymous",
+                    content: "Real Programmers don't eat much Quiche")
+
+        }
+
+
+        [ quote: randomQuote ]
+    }
+
+//    def random() {
+//        def staticAuthor = "Anonymous"
+//        def staticContent = "Real Programmers don't eat much quiche"
+//        [ author: staticAuthor, content: staticContent]
+//    }
+
+    def random2() {
+        def staticAuthor = "Anonymous"
+        def staticContent = "Real Programmers don't eat much quiche"
+        [ author: staticAuthor, content: staticContent]
+    }
+
+}
